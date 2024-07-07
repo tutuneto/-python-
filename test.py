016d5331d2e9db9795b19db29e4c42379d87ba67
Tutuneto <1491418102@qq.com>
2024-07-07 17:17:15 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+
+name = ""
+while name != 'amy':
+    print ("type your name in a right way.")
+    name = input()
+print ("Thank you.")
+
+while True:
+    print("who are you?(pay attention to capitalization)") 
+    name = input()
+    if name == "Gua":
+        print("hello, Gua. what is the key word?")
+    else:
+        print("sorry for the wrong name,plz try again.")
+    password = input()
+    if password == "love cc":
+        break
+print("congratulations!")
+
